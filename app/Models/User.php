245ed1612d786831bae9b80016a1da1af9af362e
chenguanxi0 <491788533@qq.com>
2018-07-20 15:28:19 +0800

    邮箱验证中间件

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','email_verified'
     ];
 
     /**
@@ -23,6 +23,9 @@ class User extends Authenticatable
      *
      * @var array
      */
+    protected $casts = [
+        'email_verified' => 'boolean'
+    ];
     protected $hidden = [
         'password', 'remember_token',
     ];
