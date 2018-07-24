6f6d5549adc4f8627fb666b87a10b2838343e7fe
chenguanxi0 <491788533@qq.com>
2018-07-24 11:15:44 +0800

    触发激活邮箱

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,19 @@
 Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
-Route::get('/ttest',function (){
+Route::get('/test',function (){
 //    dd(Hash::make(111));
 });
 Route::group(['middleware' => 'auth'],function (){
-
+    //邮箱验证提示
     Route::get('/email_verify_notice','PagesController@emailVerifyNotice')->name('email_verify_notice');
+    //验证 邮箱链接
     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+    //发送邮件
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
 
+    //邮箱验证通过的路由组
     Route::group(['middleware' => 'email_verified'],function (){
-        Route::get('/test',function (){
-            return 'Your email is verified';
-        });
-        Route::get('/send',function(){
-            $user = \Illuminate\Support\Facades\Auth::user();
-            $user->notify(new \App\Notifications\EmailVerificationNotification());
-        });
+
     });
 });
