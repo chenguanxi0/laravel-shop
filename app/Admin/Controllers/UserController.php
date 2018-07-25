32651c187afea5ada277cbb88c862f27d2c7b879
chenguanxi0 <491788533@qq.com>
2018-07-25 16:39:48 +0800

    后台管理 - ૬用户列表

---
+++ app/Admin/Controllers/UserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+
+class UserController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('用户列表');
+            $content->description('description');
+
+            $content->body($this->grid());
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(User::class, function (Grid $grid) {
+            $grid->id('ID')->sortable();
+            // 创建一个列名为 用户名 的列，内容是用户的 name 字段。下面的 email() 和 created_at() 同理
+            $grid->name('用户名');
+            $grid->email('邮箱');
+            $grid->email_verified('已验证邮箱')->display(function ($value){
+                return $value ? '是' : '否';
+            });
+            $grid->created_at('注册时间');
+            $grid->disableCreateButton();
+            $grid->actions(function ($actions){
+               $actions->disableDelete();
+               $actions->disableEdit();
+            });
+            $grid->tools(function ($tools) {
+
+                // 禁用批量删除按钮
+                $tools->batch(function ($batch) {
+                    $batch->disableDelete();
+                });
+            });
+        });
+    }
+
+
+}
