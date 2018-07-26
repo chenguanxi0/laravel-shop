ed2b4333631a0c4513e12fd4186241dbb88f17e4
chenguanxi0 <491788533@qq.com>
2018-07-26 09:56:47 +0800

    sku 后台后台管理 - 商品列表

--- app/Admin/Controllers/UsersController.php
+++ app/Admin/Controllers/UsersController.php
@@ -11,7 +11,7 @@ use Encore\Admin\Layout\Content;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
 
-class UserController extends Controller
+class UsersController extends Controller
 {
     use ModelForm;
 
